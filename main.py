7365f8ab6e440f65ef1826f080a184bde9beb006
NGUYENTHANHDAT <nguyenthanhdatvn2005@gmail.com>
2025-03-27 19:45:01 +0700

    Sua lai trang login

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 import searchUser, Login, loadtodb
 app = Flask(__name__)
-app.secret_key = "lolse"
+app.secret_key = "maimoremood@123"
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     data = []
