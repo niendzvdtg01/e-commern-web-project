c66873d7c16dc4712fc771da73c7c63cb776aab4
niendzvdtg01 <152018787+niendzvdtg01@users.noreply.github.com>
2025-04-02 00:13:30 +0700

    fix bug

---
+++ test.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from flask import jsonify
+import os
+from supabase import create_client
+load_dotenv()
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+response = (supabase.table("product")
+            .select("*")
+            .execute())
+
+print("✅ Dữ liệu Supabase trả về:", response)
+# Kiểm tra lỗi từ Supabase
+
+
